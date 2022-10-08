57a7e05cb060f97b16d01c7fb58f893b1dced352
Gabriel Ribeiro Maia <gribeiromaia736@gmail.com>
2022-10-08 12:59:23 -0300

    Update basicoFuncao1.js

--- basicoFuncao1.js
+++ basicoFuncao1.js
@@ -8,3 +8,7 @@ console.log("teste");
 
   const soma = idadeSomadas(idade1, idade2);
   console.log(soma)
+
+  const nome = 'Joãozinho'
+  const idade = 13
+  console.log(`${nome} é um adolescente, e tem ${idade} anos`)
